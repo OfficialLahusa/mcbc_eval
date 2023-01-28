6025760010a407fd5a9436b301b02739b6fea44c
Lasse Huber-Saffer <lasse@huber-saffer.de>
2023-01-28 04:38:31 +0100

    Refactoring

--- cnn/eval.py
+++ cnn/eval.py
@@ -11,9 +11,10 @@ from keras.layers import BatchNormalization
 from keras.layers import ReLU
 from skimage.io import imread
 
+# Working directory
 wkdir = os.path.dirname(os.path.realpath(__file__))
 
-# Model
+# Model architecture definition
 model = Sequential()
 model.add(Conv2D(32, (5, 5), padding="same", strides=1, input_shape=(216, 384, 3), activation="relu"))
 model.add(MaxPooling2D((2, 2)))
@@ -30,30 +31,36 @@ model.add(ReLU())
 model.add(Dense(5, activation='softmax'))
 model.compile(loss='categorical_crossentropy', optimizer='Adam', metrics=['accuracy'])
 
-# Model weights
+# Model weights from file
 model.load_weights(wkdir + "/mcbc_weights.h5")
 
-
+# Labels corresponding to the CNN output layer indices
 labels = ["aquatic", "arid", "forest", "plains", "snowy"]
 
 with SimpleXMLRPCServer(("localhost", 8000)) as server:
     @server.register_function
     def handle_image(path):
         print(path)
+
+        # Load image and drop alpha channel
         img = imread(path)[:, :, 0:3]
-        img_downscaled = img
+        # Add additional wrapping dimension
+        img = np.expand_dims(img, axis=0)
 
-        img_downscaled = np.expand_dims(img_downscaled, axis=0)
+        # Directly call the model to predict the output
+        result = model(img, training=False).numpy().tolist()[0]
 
-        result = model(img_downscaled, training=False).numpy().tolist()[0]
+        # Format the result
         labeled_results = [[e, result[i]] for i, e in enumerate(labels)]
         labeled_results.sort(key=lambda x: x[1], reverse=True)
         result_strs = [f"{e[0]}: {100 * e[1]:.3f}%" for e in labeled_results]
         result_str_concat = " - ".join(result_strs)
+
+        # Print & return
         print(result_str_concat)
         return result_str_concat
 
-
+    # Run server until [CTRL]+[C] is pressed
     try:
         server.serve_forever()
     except KeyboardInterrupt:
