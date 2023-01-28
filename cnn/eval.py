848797b95df53fbd214d166309be6142ea26556b
Lasse Huber-Saffer <lasse@huber-saffer.de>
2023-01-28 02:00:47 +0100

    Implemented XMLRPC CNN evaluation

---
+++ cnn/eval.py
@@ -0,0 +1,67 @@
+import sys
+
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import Flatten
+from keras.layers import Conv2D
+from keras.layers import MaxPooling2D
+from keras.layers import Dropout
+from keras.layers import BatchNormalization
+from keras.layers import ReLU
+from keras.callbacks import EarlyStopping
+from keras.callbacks import ModelCheckpoint
+from skimage.io import imread, imsave
+from skimage.transform import resize
+from xmlrpc.server import SimpleXMLRPCServer
+import os
+import numpy as np
+import glob
+
+wkdir = os.path.dirname(os.path.realpath(__file__))
+
+# Model
+model = Sequential()
+model.add(Conv2D(32, (5, 5), padding="same", strides=1, input_shape=(216, 384, 3), activation="relu"))
+model.add(MaxPooling2D((2, 2)))
+model.add(Conv2D(64, (5, 5), padding="same", strides=1, activation="relu"))
+model.add(MaxPooling2D((2, 2)))
+model.add(Conv2D(128, (5, 5), padding="same", strides=1, activation="relu"))
+model.add(MaxPooling2D((2, 2)))
+model.add(Conv2D(256, (5, 5), padding="same", strides=1, activation="relu"))
+model.add(MaxPooling2D((2, 2)))
+model.add(Flatten())
+model.add(Dense(256, use_bias=False))
+model.add(BatchNormalization())
+model.add(ReLU())
+model.add(Dense(5, activation='softmax'))
+model.compile(loss='categorical_crossentropy', optimizer='Adam', metrics=['accuracy'])
+
+# Model weights
+model.load_weights(wkdir + "/mcbc_weights.h5")
+
+
+labels = ["aquatic", "arid", "forest", "plains", "snowy"]
+
+with SimpleXMLRPCServer(("localhost", 8000)) as server:
+    @server.register_function
+    def handleImage(path):
+        print(path)
+        img = imread(path)[:, :, 0:3]
+        img_downscaled = img
+
+        img_downscaled = np.expand_dims(img_downscaled, axis=0)
+
+        result = model(img_downscaled, training=False).numpy().tolist()[0]
+        labeled_results = [[e, result[i]] for i, e in enumerate(labels)]
+        labeled_results.sort(key=lambda x: x[1], reverse=True)
+        result_strs = [f"{e[0]}: {100 * e[1]:.3f}%" for e in labeled_results]
+        result_str_concat = " - ".join(result_strs)
+        print(result_str_concat)
+        return result_str_concat
+
+
+    try:
+        server.serve_forever()
+    except KeyboardInterrupt:
+        print("\nKeyboard interrupt received, exiting.")
+        sys.exit(0)
---
+++ src/main/java/com/lahusa/mcbc_eval/MCBCEvalClient.java
@@ -0,0 +1,102 @@
+package com.lahusa.mcbc_eval;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.fabricmc.fabric.api.client.rendering.v1.WorldRenderEvents;
+import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.client.gui.hud.ChatHud;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import net.minecraft.client.util.ScreenshotRecorder;
+import net.minecraft.text.Text;
+import net.minecraft.util.Util;
+import org.apache.xmlrpc.XmlRpcException;
+import org.apache.xmlrpc.client.*;
+import org.apache.xmlrpc.common.XmlRpcHttpRequestConfigImpl;
+import org.apache.xmlrpc.common.XmlRpcRequestProcessor;
+import org.lwjgl.glfw.GLFW;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.TimeZone;
+
+@net.fabricmc.api.Environment(net.fabricmc.api.EnvType.CLIENT)
+public class MCBCEvalClient implements ClientModInitializer {
+
+    private static XmlRpcClient rpcClient;
+    private static KeyBinding classificationKey;
+    private static int stage = 0;
+    private static int frameCounter = 0;
+
+    @Override
+    public void onInitializeClient() {
+        rpcClient = new XmlRpcClient();
+        XmlRpcClientConfigImpl rpcClientConfig = new XmlRpcClientConfigImpl();
+        try {
+            rpcClientConfig.setServerURL(new URL("http://127.0.0.1:8000"));
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
+        rpcClient.setConfig(rpcClientConfig);
+
+        // Keybind registration
+        classificationKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+                "key.mcbc_datagen.classify", // The translation key of the keybinding's name
+                InputUtil.Type.KEYSYM, // The type of the keybinding, KEYSYM for keyboard, MOUSE for mouse.
+                GLFW.GLFW_KEY_O, // The keycode of the key
+                "category.mcbc_datagen.keybinds" // The translation key of the keybinding's category.
+        ));
+
+        WorldRenderEvents.END.register(
+                context -> {
+                    if(stage == 1) ++frameCounter;
+                }
+        );
+
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            ChatHud chat = client.inGameHud.getChatHud();
+
+            if (classificationKey.isPressed()) {
+                if(stage == 0) {
+                    client.getWindow().toggleFullscreen();
+                    client.getWindow().setWindowedSize(384, 216);
+                    chat.clear(false);
+                    stage = 1;
+                    frameCounter = 0;
+                }
+
+            }
+            if(stage == 1 && frameCounter > 1) {
+                String filename = "eval-"+Util.getFormattedCurrentTime() + "-" + System.currentTimeMillis()%1000 + ".png";
+                String filepath = FabricLoader.getInstance().getGameDir().toFile().toString()+"\\screenshots\\"+filename;
+                stage = 2;
+
+                ScreenshotRecorder.saveScreenshot(
+                        FabricLoader.getInstance().getGameDir().toFile(),
+                        filename,
+                        client.getFramebuffer(),
+                        (message) -> {
+                            chat.addMessage(Text.literal("Evaluating screenshot \""+filepath+"\""));
+                            client.getWindow().toggleFullscreen();
+                            try {
+                                String result = (String) rpcClient.execute(
+                                        "handleImage",
+                                        new Object[] {
+                                                 filepath
+                                        }
+                                );
+                                chat.addMessage(Text.literal(result));
+                            } catch (XmlRpcException e) {
+                                throw new RuntimeException(e);
+                            }
+                            stage = 3;
+                        }
+                );
+            }
+            else if(stage == 3) {
+                stage = 0;
+            }
+        });
+    }
+}
